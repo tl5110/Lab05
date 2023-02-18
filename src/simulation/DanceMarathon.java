e83ddfb140e6a8739d1d9687659e7ae80da72a9d
Tiffany Lee <tl5110@g.rit.edu>
2023-02-17 21:22:00 -0500

    Added docstring to DanceMarathon

--- src/simulation/DanceMarathon.java
+++ src/simulation/DanceMarathon.java
@@ -7,11 +7,45 @@ import java.util.Random;
 import java.io.FileNotFoundException;
 import java.util.stream.Collectors;
 
+/**
+ * The main program is run on the command line with a tracks file:
+ *
+ * Usage: java DanceMarathon filename
+ *
+ * 1. The songs are read from a file into both a hashmap/jukebox, with
+ *      the song's artist and title as a key and the value as the amount
+ *      of time it's played, and a list too. Displays the total number
+ *      of songs loaded into the hashmap/jukebox
+ * 2. The songs are randomly picked to be played. Until a song is replayed,
+ *      each song is added to a hashset and the value of the time a song is
+ *      played is increased in the hashmap. This process is repeated 100000
+ *      times. The first five songs played and the total time in seconds it
+ *      took to run the entire simulation are displayed.
+ * 3. Some statistics are displayed about the amount of times the simulation
+ *      is run, the total number of songs played throughout the entire
+ *      simulation, the average number of songs played to get a duplicate
+ *      across the entire simulation, the song that was played the most, and
+ *      a list of all the songs by the most played song's artist in
+ *      alphabetical order with the total number of times each song was played
+ *
+ * @author Tiffany Lee
+ */
 public class DanceMarathon {
+    /**
+     * the jukebox that keeps track of how
+     * many times each song has been played
+     */
     private final HashMap<Song,Integer> jukebox = new HashMap<>();
+    /** the list of all the songs in the jukebox */
     private final List<Song> playlist = new ArrayList<>();
-    private final Random songIndex = new Random(42);
 
+    /**
+     * Reads the data file of songs and loads them all into the jukebox.
+     * Displays the first and last song in the jukebox.
+     *
+     * @param filename name of the file
+     * @throws FileNotFoundException if the file is not found
+     */
     public DanceMarathon(String filename) throws FileNotFoundException {
         Scanner tracks = new Scanner(new File(filename));
 
@@ -30,16 +64,23 @@ public class DanceMarathon {
         System.out.println("\tLast song in jukebox: " + playlist.get(playlist.size()-1));
     }
 
+    /**
+     * Randomly accesses songs in the jukebox.
+     * If a song is played its value of plays in the jukebox increases
+     * and is continuously added to hash-set until a song has been replayed.
+     * This process is repeated 100,000 times.
+     * Prints out when the simulation is running, the first five songs played,
+     * and the amount of time the simulation took in seconds.
+     */
     public void simulation(){
         System.out.println("Running the simulation.  The jukebox starts rockin'!");
         List<Song> five = new ArrayList<>();
+        Random songIndex = new Random(42);
         long startTime = System.currentTimeMillis();
         for(int i = 0; i < 100000; i++){
             HashSet<Song> replay = new HashSet<>();
             Song a = playlist.get(songIndex.nextInt(playlist.size()));
-//            replay.stream().forEach(song -> song.equals(a));
             while(!replay.contains(a)){
-//                five.stream().limit(5).collect(Collectors.toList((ArrayList::add(a))));
                 if(five.size()!=5){
                     five.add(a);
                 }
@@ -55,6 +96,14 @@ public class DanceMarathon {
         System.out.println("\tSimulation took " + totalTime + " second/s to run");
     }
 
+    /**
+     * Determines and displays the number of simulation runs,
+     * total number of songs played throughout the simulation,
+     * average amount of plays it took to get a duplicate across
+     * the entire simulation, finds the most played song,and all
+     * the other songs, and amount of time they are played, by
+     * the artist of the most played song in alphabetical order.
+     */
     public void statistics(){
         int totalPlays = jukebox.values().stream().reduce(0, Integer::sum);
         int avgPlays = (int) Math.ceil((double)totalPlays/100000);
@@ -75,6 +124,15 @@ public class DanceMarathon {
                 + "\" with " + jukebox.get(song) + " plays"));
     }
 
+    /**
+     * The main method that gets the filename of a file of songs from the
+     * command line arguments, creates a jukebox from file of songs,
+     * runs a simulation of it, and calculates its statistics.
+     *
+     * @param args command line arguments that contains filename
+     *             of a file of songs
+     * @throws FileNotFoundException if the file is not found
+     */
     public static void main(String[] args) throws FileNotFoundException {
         if (args.length != 1) {
             System.out.println("Usage: java DanceMarathon filename");
